c9242b3971bcfe970ed60b99c4bf5815cfa24f24
Ranulfo Mascari Neto <ranulfomascarineto@Ranulfos-MacBook-Air.local>
2023-04-21 20:16:34 -0300

    Inserção da resolução e descrição do problema

---
+++ Beecrowd/VII Maratona Mineira de Programação/Feijão/2791.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> vi;
+
+int main() {
+    int n = 4;
+    vi C(n, 0);
+    int feijao;
+    for (int i = 0; i < n; i++) {
+        cin >> C[i];
+        if (C[i]) {
+            feijao = i;
+        }
+    }
+    cout << feijao+1;
+    return 0;
+}
