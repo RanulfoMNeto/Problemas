971958eb3ded885322d05241afc83f1e0f493e2e
Ranulfo Mascari Neto <ranulfomascarineto@Ranulfos-MacBook-Air.local>
2023-05-13 00:46:58 -0300

    Alteração das condicionais

--- Beecrowd/Labirinto/1621.cpp
+++ Beecrowd/Labirinto/1621.cpp
@@ -38,43 +38,30 @@ int bfs(vvc &LA, int N, int M, int i, int j) {
 		fila.pop();
 
         char v = LA[i][j];
-        if ((i > 0) && (LA[i-1][j]) && (visitados[i-1][j] == false) && (LA[i-1][j] == '.')) {
+        if ((i > 0) && (visitados[i-1][j] == false) && (LA[i-1][j] == '.')) {
             visitados[i-1][j] = true;
             d[i-1][j] = d[i][j] + 1;
             maior = d[i-1][j];
             fila.push(ii(i-1, j));
         }
-        if ((j < M-1) && (LA[i][j+1]) && (visitados[i][j+1] == false) && (LA[i][j+1] == '.')) {
+        if ((j < M-1) && (visitados[i][j+1] == false) && (LA[i][j+1] == '.')) {
             visitados[i][j+1] = true;
             d[i][j+1] = d[i][j] + 1;
             maior = d[i][j+1];
             fila.push(ii(i, j+1));
         }
-        if ((i < N-1) && (LA[i+1][j]) && (visitados[i+1][j] == false) && (LA[i+1][j] == '.')) {
+        if ((i < N-1) && (visitados[i+1][j] == false) && (LA[i+1][j] == '.')) {
             visitados[i+1][j] = true;
             d[i+1][j] = d[i][j] + 1;
             maior = d[i+1][j];
             fila.push(ii(i+1, j));
         }
-        if ((j > 0) && (LA[i][j-1]) && (visitados[i][j-1] == false) && (LA[i][j-1] == '.')) {
+        if ((j > 0) && (visitados[i][j-1] == false) && (LA[i][j-1] == '.')) {
             visitados[i][j-1] = true;
             d[i][j-1] = d[i][j] + 1;
             maior = d[i][j-1];
             fila.push(ii(i, j-1));
         }
-        /*
-        for (int k = 0; k < N; k++) {
-            for (int l = 0; l < M; l++) {
-                if (d[k][l] == INF) {
-                    cout << "+";
-                } else {
-                    cout << d[k][l];
-                }
-            }
-            cout << endl;
-        }
-        cout << endl;
-        */
 	}
 
     return maior;
@@ -101,8 +88,8 @@ int main() {
         for (int i = 0; i < N; i++) {	
             for (int j = 0; j < M; j++) {
                 if (LA[i][j] == '.') {
-                    // encontra a saída do labirinto
-                    int maiorLocal = bfs(LA, N, M, i, j);
+                    // encontra a saída do labirinto com a maior distância
+                    int maiorLocal = bfs(LA, N, M, i, j); // retorna a distância
                     if (maiorLocal > maior) {
                         maior = maiorLocal;
                     }
